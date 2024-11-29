5ab6fc2126634b869eb1de2db370992e532a4174
ChandanaManjunath015 <chandanamanjunath015@gmail.com>
2024-11-30 01:39:29 +0530

    Update assignment1.c

--- assignment1.c
+++ assignment1.c
@@ -4,90 +4,116 @@
 #include <ctype.h>
 
 int main() {
-
-	
     char a[100];
     printf("Enter your expression\n");
     fgets(a, sizeof(a), stdin);
 
     int len = strlen(a);
-    int cur = 0;
-    int  res = 0;
-    int temp = 0;
-    char end_op = '+';
-    int last=1;
-    int i=0;
-    while (i < len)
+    char processed[100] = {0};
+    int end_op = 1;
+    int j=0;
+    for (int i=0;i<len;i++)
     {
-        if(isspace(a[i]))
+        if (isspace(a[i]))
         {
-            i++;
             continue;
         }
-
-        if (isdigit(a[i]))
+        else if(isdigit(a[i]))
         {
-            cur = 0;
-	        last=0;
-            while (a[i] >= '0' && a[i] <= '9' && i<len)
-            {
-                int d = a[i] - '0';
-                cur = cur * 10 + d;
-
-                i++;
-            }
-            if (end_op == '*')
-            {
-                temp = temp*cur;
-            }
-
-
-           else if (end_op == '/')
-	        {
-                if (cur == 0)
-		        {
-
-                    printf("Error: Division by zero.\n");
-                    return 1;
-                }
-                temp = temp/cur;
-            }
-            else if (end_op == '+')
-            {
-                res = res+temp;
-                temp = cur;
-            }
-            else if (end_op == '-')
-            {
-                res = res+temp;
-                temp = -cur;
-            }
-
+            processed[j++] = a[i];
+            end_op= 0;
         }
-	    else if (a[i] == '+' || a[i] == '-' || a[i] == '*' || a[i] == '/')
-	    {
-		if(last)
+        else if (a[i] == '+'|| a[i]=='-'||a[i]=='*'||a[i]=='/')
         {
-            printf("Error: Invalid expression.\n");
-            return 1;
+            if (end_op)
+            {
+              printf("Error: Invalid expression.\n");
+              return 1;
+            }
+            processed[j++] = a[i];
+            end_op= 1;
         }
-        last=1;
-        end_op = a[i];
-        i++;
-        }
+
+
         else
         {
+
             printf("Error: Invalid expression.\n");
             return 1;
         }
     }
-    if(last)
+
+
+
+    if (end_op)
     {
+
         printf("Error: Invalid expression.\n");
         return 1;
     }
-    res=res+temp;
 
-    printf("%d\n", res);
+    processed[j] = '\0';
+
+
+    int temp = 0;
+    int res = 0;
+    char op = '+';
+    int num = 0;
+    char prev_op = '+';
+
+    len = strlen(processed);
+    for (int i = 0; i <= len; i++)
+    {
+        if (isdigit(processed[i]))
+        {
+            num=num*10+(processed[i]-'0');
+        }
+        else if (processed[i]=='+'||processed[i]=='-'||processed[i]=='*'||processed[i]=='/'||processed[i]=='\0')
+        {
+
+            if (op=='*')
+            {
+                temp=temp*num;
+            }
+            else if (op=='/')
+            {
+                if (num == 0)
+                {
+                    printf("Error: Division by zero.\n");
+                    return 1;
+                }
+                temp=temp/num;
+            }
+            else
+            {
+                if (prev_op=='+')
+                {
+                    res=res+temp;
+                }
+                else if (prev_op=='-')
+                {
+                    res-= temp;
+                }
+                temp = num;
+                prev_op = op;
+            }
+
+
+            op = processed[i];
+            num = 0;
+        }
+    }
+
+
+    if (prev_op=='+')
+    {
+        res=res+temp;
+    }
+    else if (prev_op=='-')
+    {
+        res=res-temp;
+    }
+
+    printf("%d\n",res);
     return 0;
 }
