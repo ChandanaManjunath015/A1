1fb70504e24e99037c6f66babbc417e2aad0cfe7
ChandanaManjunath015 <chandanamanjunath015@gmail.com>
2024-12-25 02:16:35 +0530

    Update assigment1_calculator_updated.c

--- assigment1_calculator_updated.c
+++ assigment1_calculator_updated.c
@@ -1,80 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-int isDigit(char c) 
+
+int isDigit(char expression)
 {
-    return c >= '0' && c <= '9';
+    return expression >= '0' && expression <= '9';
 }
+
 int stringLength(char* str)
  {
     int length = 0;
-    while (str[length] != '\0') 
+    while (str[length] != '\0')
     {
         length++;
     }
     return length;
 }
-int stringCompare(char* str1, char* str2) 
+
+int stringCompare(char* str1, char* str2)
 {
     int i = 0;
-    while (str1[i] != '\0' && str2[i] != '\0') 
+    while (str1[i] != '\0' && str2[i] != '\0')
     {
-        if (str1[i] != str2[i]) 
+        if (str1[i] != str2[i])
         {
-            return 0; 
+            return 0;
         }
         i++;
     }
-    return str1[i] == str2[i];  
+    return str1[i] == str2[i];
 }
-void removeNewline(char* str) 
+
+void removeNewline(char* str)
 {
     int len = stringLength(str);
-    if (str[len - 1] == '\n') 
+    if (str[len - 1] == '\n')
     {
         str[len - 1] = '\0';
     }
 }
-int processExpression(char* expression, char* processedExpression) 
+
+int processExpression(char* expression, char* processedExpression)
 {
     int len = stringLength(expression);
-    int endOp = 1; 
+    int endOp = 1;
     int j = 0;
-    int invalid = 0;  
-    for (int i = 0; i < len; i++) 
+    int invalid = 0;
+    for (int i = 0; i < len; i++)
     {
-        if (expression[i] == ' ') 
+        if (expression[i] == ' ')
         {
-            continue; 
-        } 
-        else if (isDigit(expression[i])) 
+            continue;
+        }
+        else if (isDigit(expression[i]))
         {
             processedExpression[j++] = expression[i];
             endOp = 0;
-        } 
-        else if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/') 
+        }
+        else if (expression[i] == '+' || expression[i] == '-' || expression[i] == '*' || expression[i] == '/')
         {
-            if (endOp) 
+            if (endOp)
             {
                 invalid = 1;
-                break;  
+                break;
             }
             processedExpression[j++] = expression[i];
             endOp = 1;
         }
-        else 
+        else
         {
             invalid = 1;
-            break; 
+            break;
         }
     }
     processedExpression[j] = '\0';
-    if (endOp || invalid) 
+    if (endOp || invalid)
     {
-        return 1; 
+        return 1;
     }
-    return 0; 
+    return 0;
 }
-int evaluateExpression(char* processedExpression) 
+
+int evaluateExpression(char* processedExpression)
 {
     int temp = 0;
     int result = 0;
@@ -82,36 +88,36 @@ int evaluateExpression(char* processedExpression)
     int num = 0;
     char prevOp = '+';
     int len = stringLength(processedExpression);
-    int errorOccurred = 0; 
-    for (int i = 0; i <= len; i++) 
+    int errorOccurred = 0;
+    for (int i = 0; i <= len; i++)
     {
-        if (isDigit(processedExpression[i])) 
+        if (isDigit(processedExpression[i]))
         {
             num = num * 10 + (processedExpression[i] - '0');
-        } 
+        }
         else if (processedExpression[i] == '+' || processedExpression[i] == '-' || processedExpression[i] == '*' || processedExpression[i] == '/' || processedExpression[i] == '\0')
         {
-            if (op == '*') 
+            if (op == '*')
             {
                 temp = temp * num;
-            } 
-            else if (op == '/') 
+            }
+            else if (op == '/')
             {
-                if (num == 0) 
+                if (num == 0)
                 {
                     printf("Error: Division by zero.\n");
                     errorOccurred = 1;
-                    break; 
+                    break;
                 }
                 temp = temp / num;
             }
             else
             {
-                if (prevOp == '+') 
+                if (prevOp == '+')
                 {
                     result = result + temp;
-                } 
-                else if (prevOp == '-') 
+                }
+                else if (prevOp == '-')
                 {
                     result = result - temp;
                 }
@@ -122,50 +128,51 @@ int evaluateExpression(char* processedExpression)
             num = 0;
         }
     }
-    if (errorOccurred) 
+    if (errorOccurred)
     {
-        return -1;  
+        return -1;
     }
-    if (prevOp == '+') 
+    if (prevOp == '+')
     {
         result = result + temp;
-    } 
-    else if (prevOp == '-') 
+    }
+    else if (prevOp == '-')
     {
         result = result - temp;
     }
     return result;
 }
-int main() 
-{
-    char expression[100];  
-    int continueRunning = 1;  
 
-    while (continueRunning) 
+int main()
+{
+    char expression[100];
+    int continueRunning = 1;
+
+    while (continueRunning)
     {
         printf("Enter your expression (or type 'exit' to quit):\n");
         int i = 0;
         char ch;
-        while ((ch = getchar()) != '\n' && ch != EOF) 
+        while ((ch = getchar()) != '\n' && ch != EOF)
         {
             expression[i++] = ch;
         }
         expression[i] = '\0';
-        if (stringCompare(expression, "exit") == 1) 
+        if (stringCompare(expression, "exit") == 1)
         {
-            continueRunning = 0; 
+            continueRunning = 0;
             break;
         }
         char processedExpression[100] = {0};
-        if (processExpression(expression, processedExpression)) 
+        if (processExpression(expression, processedExpression))
         {
             printf("Error: Invalid expression.\n");
-            continue;  
+            continue;
         }
         int result = evaluateExpression(processedExpression);
-        if (result == -1) 
+        if (result == -1)
         {
-            continue; 
+            continue;
         }
         printf("Result: %d\n", result);
     }
